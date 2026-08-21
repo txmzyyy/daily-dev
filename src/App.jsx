8dd9a978564b1034b9e78544a002649861230ce4
jamescoder92 <kusimbajames92@gmail.com>
2026-08-21 23:38:03 +0300

    app.jsx and main.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppRoutes from './routes/AppRoutes';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#09090b]">
+      <AppRoutes />
+    </div>
+  );
+}
