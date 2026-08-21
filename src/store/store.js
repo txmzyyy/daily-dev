5a3f0183282957fa5b056f7145575ba81787fffb
jamescoder92 <kusimbajames92@gmail.com>
2026-08-22 00:11:48 +0300

    store.js code

---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/authSlice';
+import contentReducer from '../features/content/contentSlice';
+import categoryReducer from '../features/categories/categorySlice';
+import moderationReducer from '../features/moderation/moderationSlice';
+import notificationReducer from '../features/notifications/notificationSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    content: contentReducer,
+    categories: categoryReducer,
+    moderation: moderationReducer,
+    notifications: notificationReducer,
+  },
+});
